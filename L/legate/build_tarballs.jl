1f61df5b9a1a004affbb9f44f47df1826e8a82ff
krasow <krasow@u.northwestern.edu>
2026-02-01 00:44:28 -0600

    Update legate to 26.01.00

--- L/legate/build_tarballs.jl
+++ L/legate/build_tarballs.jl
@@ -12,9 +12,9 @@ include(joinpath(YGGDRASIL_DIR, "platforms", "mpi.jl"))
 include("make_script.jl")
 
 name = "legate"
-version = v"25.10.2" # Year.Month
+version = v"26.01.00" # Year.Month
 sources = [
-    GitSource("https://github.com/nv-legate/legate.git","b0a20719ce9c5f63e8b1c3ddd1e592af5e7a3df6"),
+    GitSource("https://github.com/nv-legate/legate.git","3ccb639605eecd8e9fee52c2d7d56ea799f4864e"),
     DirectorySource("./bundled"),
     FileSource("https://repo.anaconda.com/miniconda/Miniconda3-py311_24.3.0-0-Linux-x86_64.sh", 
                 "4da8dde69eca0d9bc31420349a204851bfa2a1c87aeb87fe0c05517797edaac4", "miniconda.sh")
